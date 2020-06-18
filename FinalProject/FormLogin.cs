29fe963ed355c85897d0cb13cfb8ad28fc404950
letandat2303 <ddat705@gmail.com>
2020-06-18 23:19:12 +0700

    dat

---
+++ FinalProject/FormLogin.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                Form f = new Form1();
+                this.Hide();
+                f.ShowDialog();
+                this.Show();
+            }
+        }
+    }
+}
